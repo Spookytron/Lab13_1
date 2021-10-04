130363b2b25b251268d452a10226f834d6791227
Cameron Power <cameronpower@quickenloans.com>
2021-10-04 16:55:28 -0400

    starting attempt at lab 13_1

---
+++ Lab13_1/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab13_1.Models
+{
+    public class User
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string confirm { get; set; }
+
+    }
+}
